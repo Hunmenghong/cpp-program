c47644e6728ff8f406516fc00b51ba4730ea3623
Hunmenghong <hunmenghong01@gmail.com>
2024-12-19 13:23:24 +0700

    new code

--- condition/minimum.cpp
+++ condition/minimum.cpp
@@ -1,2 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
+int main(){
+
+    int number1;
+    int number2;
+    int minimum;
+
+    cout <<"Enter value of number 1 :";
+    cin >>number1;
+    cout <<"Enter value of number 2 :";
+    cin >>number2;
+
+     cout<<"-----------------------------"<<endl;
+
+    if (number1 > number2)
+    {
+        minimum =number2;
+    }
+    if (number1 < number2)
+    {
+        minimum = number1;
+        
+    }
+    
+    cout<<"Minimum = "<<minimum<<endl;
+    
+
+
+
+}
